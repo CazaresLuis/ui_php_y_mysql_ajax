e41376153d11be53027f1aa27950c1369a11c278
Luis Fernando Cázares Bulbarela <luis.f.cazares@gmail.com>
2013-03-12 23:47:43 -0600

    Actualización recuperación de contraseña parte II
    Recuperación de contraseña parte II (código php)

--- includes/appResponses.inc.php
+++ includes/appResponses.inc.php
@@ -10,6 +10,8 @@ $appResponse = array(
 	"contenido" => ""
 );
 
+$root = '../';
+
 // Verificamos las variables post y que exista la variable accion
 if(isset($_POST) && !empty($_POST) && isset($_POST['accion'])){
 
@@ -25,7 +27,47 @@ if(isset($_POST) && !empty($_POST) && isset($_POST['accion'])){
 			$appResponse['mensaje'] = "Usuario Encontrado";
 
 		break;
-		
+
+		case 'recuperaPass':
+			// verificar variable de correo que no este vacía
+			if(!empty($_POST['rec_correo'])){
+				// Verificamos que exista la cuentad e correo electrónico en nuestra tabla de usuarios
+				if(verificaCorreo($_POST['rec_correo'],$mysqli)){
+
+					// Crear clave de acceso
+					$_POST['usr_pass'] = creaPassword();
+
+					// Actulizamos el password en la tabla
+					if(actualizaClave($_POST,$mysqli)){
+						// Enviamos por correo electrónico la clave
+
+						// Cargamos e inicializamos el phpmailer
+						require_once($root.'php_libs/PHPMailer_v5.1/class.phpmailer.php');
+
+						// Creamos el objeto mail
+						$mail = new PHPMailer();
+
+						if(mandaCorreo($mail,$_POST)){
+
+							$appResponse['respuesta'] = true;
+							$appResponse['mensaje'] = "Se envió correctamente la clave de aceso";
+
+						}else{
+							$appResponse['mensaje'] = "Se creo correctamente la contraseña pero no se realizó el evnío de la misma por correo electrónico";
+						}
+
+					}
+					else{
+						$appResponse['mensaje'] = "No se puede actualziar la contraseña del usuario";
+					}
+
+				}
+				else{
+					$appResponse['mensaje'] = "Usuario no encontrado";
+				}
+			}
+		break;
+
 		default:
 			$appResponse['mensaje'] = "Opción no disponible";
 		break;
--- includes/mainFunctions.inc.php
+++ includes/mainFunctions.inc.php
@@ -41,6 +41,114 @@ function userLogin($data,$dbLink){
 	return $respuestaLogin;
 }
 
+// Función para verificar la existencia del correo electrónico en la tabla de usuarios
+function verificaCorreo($data,$dbLink){
+	// Bandera de logueo
+	$response = false;
+
+	// validar si hay datos en los parametros
+	if(!empty($data) && !empty($dbLink)){
+
+		$consulta = sprintf("SELECT * FROM tbl_users
+							WHERE usr_email='%s' AND usr_activo=1
+							LIMIT 1",
+							trim($data));
+
+		// Ejecutamos la cosnulta
+		$respuesta = $dbLink -> query($consulta);
+
+		// verificamos si es exitoso el login
+		if($respuesta -> num_rows != 0){
+			$response = true;
+		}
+
+	}
+
+	return $response;
+}
+
+// Función para crear una cadena aleatoria
+function creaPassword( $length = 10 )
+{
+	$key = "";
+	$pattern = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789";
+
+	for($i = 0 ; $i < $length ; $i++ )
+	{
+		$key .= $pattern[rand(0,53)];
+	}
+
+	return $key;
+}
+
+
+// Actualizar la contraseña del usuario en la tabla
+function actualizaClave($data,$dbLink){
+	$consulta = sprintf("UPDATE tbl_users SET 
+						usr_pass='%s'
+						WHERE usr_email='%s' LIMIT 1",
+						md5($data['usr_pass']),$data['rec_correo']);
+	// Ejecutamos la cosnulta
+	$respuesta = $dbLink -> query($consulta);
+
+	if($dbLink -> affected_rows == 1){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+// función para realizar envío de correo electrónico
+function mandaCorreo($mail,$data=''){
+
+	$response = false;
+
+	$destNombre = 'Recuperación de Contraseña';
+
+	$body = '
+		<div>
+			<p></strong> su contraseña de acceso ha sido modificada </strong>, A continuación le indicamos como accesar al sistema.</p>
+			<p><strong>DATOS DEL USUARIO</strong></p>
+            <p><strong>Usuario: '. $data['rec_correo'] . '</p>
+            <p><strong>Clave: '. $data['usr_pass'] . '</p>
+		</div>
+	';
+
+	$mail->IsSMTP(); // telling the class to use SMTP
+	$mail->Host       = "mail.worldcargoexport.com"; // SMTP server
+	// $mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+	                                           // 1 = errors and messages
+	                                           // 2 = messages only
+	$mail->SMTPAuth   = true;                  // enable SMTP authentication
+	$mail->Host       = "mail.worldcargoexport.com"; // sets the SMTP server
+	$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
+	$mail->Username   = "alertas-no-reply@worldcargoexport.com"; // SMTP account username
+	$mail->Password   = "06107210";        // SMTP account password
+
+	$mail->SetFrom('alertas-no-reply@worldcargoexport.com', '.:: Interfaz de Usuario ::.');
+
+	$mail->AddReplyTo('alertas-no-reply@worldcargoexport.com', '.:: Interfaz de Usuario ::.');
+
+	$mail->Subject    = utf8_decode(".:: Interfaz de Usuario ::. Recuperación de contraseña");
+
+	$mail->AltBody    = "Para ver el mensaje, utilice un cliente de correo electrónico compatible con HTML!!!!"; // optional, comment out and test
+
+	$mail->MsgHTML($body);
+
+	$address = $data['rec_correo'];
+	
+	$mail->AddAddress($address, $destNombre);
+	// $mail->AddBCC('luis.f.cazares@gmail.com','Luis Fernando Cázares');
+
+	if($mail->Send()) {
+	  $response = true;
+	}
+
+	return $response;
+}
+
+
 // Verificar constantes para conexión al servidor
 if(defined('server') && defined('user') && defined('pass') && defined('mainDataBase'))
 {
