a1707fe7dc1121f510fd0fc405b3e731a5925e3d
Luis Fernando Cázares Bulbarela <luis.f.cazares@gmail.com>
2013-03-12 23:50:40 -0600

    Actualización de tabla de usuarios
    Creación de campo unique usr_email

--- docs/tbl_users.sql
+++ docs/tbl_users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 26-01-2013 a las 02:13:05
+-- Tiempo de generación: 13-03-2013 a las 06:48:42
 -- Versión del servidor: 5.5.25
 -- Versión de PHP: 5.4.4
 
@@ -33,7 +33,8 @@ CREATE TABLE `tbl_users` (
   `usr_email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `usr_pass` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `usr_activo` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 Inhabilitado, 1 Habilitado',
-  PRIMARY KEY (`idUsuario`)
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `usr_email` (`usr_email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
@@ -41,7 +42,7 @@ CREATE TABLE `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`idUsuario`, `usr_nombre`, `usr_apellidos`, `usr_email`, `usr_pass`, `usr_activo`) VALUES
-(1, 'Luis Fernando', 'Cázares Bulbarela', 'luis.f.cazares@gmail.com', 'e807f1fcf82d132f9bb018ca6738a19f', 1);
+(1, 'Luis Fernando', 'Cázares Bulbarela', 'luis.f.cazares@gmail.com', 'ca4e66fd657fa3fb6cd2a19ae0d3f861', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
